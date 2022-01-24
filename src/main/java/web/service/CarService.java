0beefa1635955e4aeb6c6f61c212a257028db828
IrinaRazumova <irina.v.razumova@gmail.com>
2022-01-24 13:52:26 +0300

    CarList() переделан на Stream

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -2,45 +2,20 @@ package web.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import web.car.Car;
 
 public class CarService {
     public  static List<Car> getCarList(String count) {
-        Car c1 = new Car("Mersedes", "red", "111");
-        Car c2 = new Car("BMW", "blue", "222");
-        Car c3 = new Car("Volvo", "black", "333");
-        Car c4 = new Car("Audi", "yellow", "444");
-        Car c5 = new Car("ЗАЗ", "green", "555");
-
         List<Car> carList = new ArrayList<>();
-        switch (count) {
-            case ("1"):
-                carList.add(c1);
-                break;
-            case ("2"):
-                carList.add(c1);
-                carList.add(c2);
-                break;
-            case ("3"):
-                carList.add(c1);
-                carList.add(c2);
-                carList.add(c3);
-                break;
-            case ("4"):
-                carList.add(c1);
-                carList.add(c2);
-                carList.add(c3);
-                carList.add(c4);
-                break;
-            default:
-                carList.add(c1);
-                carList.add(c2);
-                carList.add(c3);
-                carList.add(c4);
-                carList.add(c5);
-                break;
-        }
 
-        return carList;
+        carList.add(new Car("Mersedes", "red", "111"));
+        carList.add(new Car("BMW", "blue", "222"));
+        carList.add(new Car("Volvo", "black", "333"));
+        carList.add(new Car("Audi", "yellow", "444"));
+        carList.add(new Car("ЗАЗ", "green", "555"));
+
+        return carList.stream().limit(Integer.valueOf(count)).collect(Collectors.toList());
     }
 }
